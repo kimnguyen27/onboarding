079212802b78d06e9e3caeab2f197b623b0ac4e9
Kim Nguyen <7277481+kimnguyen27@users.noreply.github.com>
2020-02-24 19:16:07 -0600

    Got Twilio SMS messaging tester working, will merge with rest of code properly later

---
+++ onboarding-service/src/main/java/com/vivvo/onboarding/TwilioSmsTester.java
@@ -0,0 +1,21 @@
+package com.vivvo.onboarding;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class TwilioSmsTester {
+
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+13065022827"), // to
+                new com.twilio.type.PhoneNumber("+13069880988"), // from
+                "Test").create();
+
+        System.out.println(message.getSid());
+    }
+}
